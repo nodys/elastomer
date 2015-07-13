3bc5cf8061948ecd92d96edbc9c9fe50100347c4
Jean Ponchon <jean.ponchon@novadiscovery.com>
2015-07-13 20:13:25 +0200

    dev: Looks pretty good

--- test/fixtures/functional/elasto-foo/index.js
+++ test/fixtures/functional/elasto-foo/index.js
@@ -13,21 +13,69 @@ function link (scope, elasto) {
   scope.val = 'Foobar'
   scope.app = {}
   scope.app.ctx = createContext()
+  scope.list = ['foo', 'bar']
   elasto.mapAttribute('val')
   elasto.setTimeout(function () {
     scope.hello = 'Foob'
     elasto.update()
   }, 1000)
 
+  // window.scope = scope
+
+  var _foo
+  Object.defineProperty(scope, 'foo', {
+    set: function (value) {
+      _foo = value
+    },
+    get: function () {
+      return _foo
+    },
+    configurable: true
+  })
+
   var inc = 1
+
+  elasto.watch('app.ctx.path', function (v) {
+    // console.log('Change on path A', v)
+  })
+
+  // elasto.watch('app.ctx.path', function (v) {
+  //   console.log('Change on path B', v)
+  // })
+  //
+  // elasto.watch('app.ctx', function () {
+  //   console.log('Change on path C', arguments)
+  // })
+  //
+  // elasto.watch('app.ctx', function () {
+  //   console.log('Change on path D', arguments)
+  // })
+  elasto.watch('list', function (val, old, splice) {
+    // console.log('Change on list', val, splice)
+  })
 
   elasto.setInterval(function () {
     scope.size = inc + parseInt(scope.size, 10)
     if (scope.size > 100 || scope.size < 0) {
       inc = -inc
     }
-    elasto.update()
   }, 50)
+
+  elasto.setInterval(function () {
+    scope.list.set(0, '_' + Math.round(Math.random() * 1000000000))
+    scope.list.set(1, '_' + Math.round(Math.random() * 1000000000))
+    if(Math.random() > .5) {
+      // scope.list[2] = '_' + Math.round(Math.random() * 1000000000)
+      scope.list.set(2, '_' + Math.round(Math.random() * 1000000000))
+    } else if(scope.list[2]) {
+      // scope.list.pop()
+    }
+    // scope.list.change()
+    // scope.list.splice(3,1, 'FOO_'  + Math.round(Math.random() * 1000000000))
+    scope.list = scope.list
+
+    // elasto.update()
+  }, 500)
 
   setInterval(function () {
     scope.app.ctx = createContext()
@@ -36,6 +84,6 @@ function link (scope, elasto) {
 
 function createContext() {
   return  {
-      path: '/' + Math.round(Math.random() * 1000000000)
+    path: '/' + Math.round(Math.random() * 1000000000)
   }
 }
--- test/fixtures/functional/elasto-layout/index.js
+++ test/fixtures/functional/elasto-layout/index.js
@@ -11,9 +11,35 @@ module.exports = elastomer('elasto-layout', {
 })
 
 function link (scope, elasto) {
-  scope.model = {
-    ctx: {
-      component: 'hub-dash'
-    }
+  // scope.model = {
+  //   ctx: {
+  //     path: '/foo'
+  //   }
+  // }
+  // window.scope = scope
+  // window.watchy = elastomer.watchy
+  //
+  // elasto.watch('model.ctx.path', function (val, old) {
+  //   console.log('watch A', val, old)
+  // })
+  //
+  // elasto.watch('model.ctx.path', function (val, old) {
+  //   console.log('watch B', val, old)
+  // })
+
+  // elasto.watch('model.ctx.path', function (val, old) {
+  //   console.log('watch B', val, old)
+  // })
+  //
+  // elasto.watch('model.ctx.path', function (val, old) {
+  //   console.log('watch C', val, old)
+  // })
+  //
+  // elasto.watch('model.ctx.path', function (val, old) {
+  //   console.log('watch D', val, old)
+  // })
+
+  scope.handleRemove = function () {
+    elasto.host.remove()
   }
 }
